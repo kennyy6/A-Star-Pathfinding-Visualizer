5dfc5ba41dd67ac5cb5eb053e5dffde41298e5f6
Kenny Y <46588600+kennyy6@users.noreply.github.com>
2020-03-15 14:41:14 +1100

    finished logic

---
+++ a_star_logic.py
@@ -0,0 +1,153 @@
+class Node:
+    def __init__(self, parent = None,position = None):
+        self.parent = parent
+        self.position = position
+
+        self.f = 0
+        self.g  = 0
+        self.h = 0
+
+
+class Logic_AStar_Algo:
+
+    def __init__(self):
+        """
+        Makes a new List of List that  is act as a board
+        """
+        self.val_list = []
+        for i in range(16):
+            a_list = []
+            for j in range(16):
+                a_list +=[0]
+            #print(a_list)
+            self.val_list += [a_list]
+
+    def print_neat(self):
+
+        for i in self.val_list:
+            print(i)
+        return None
+    def insert_Nodes(self,x,y,mode):
+        """
+        Mode: 3 = Barriers, 1= Start , 2= End
+        :param x:
+        :param y:
+        :param mode:
+        :return:
+        """
+        if mode == 1:
+            self.val_list[y][x] = 1 # 1 Represents start
+            self.start_node = Node(None,(y,x))
+        elif mode == 2:
+            print(x,y)
+            self.val_list[y][x] = 2
+            self.end_node = Node(None, (y, x))
+        else:
+            self.val_list[y][x] = 3
+
+    def pathFinding(self,gui_reference):
+        # Using gui reference here so we can map out the calculations it performs
+
+
+        #Intialize open and closed list
+        open_list = []
+        close_list = []
+
+        open_list.append(self.start_node)
+
+        while len(open_list) > 0:
+            test1 = []
+            test2 = []
+            # for i in open_list:
+            #     test1 += [i.position,i.f]
+            # for j in close_list:
+            #     test2 += [j.position,i.f]
+            # print(test1,"tesst",test2)
+            current_node = open_list[0]
+            current_index = 0
+
+            for index, item in enumerate(open_list):
+                if item.f < current_node.f:
+                    current_node = item
+                    current_index = index
+
+            open_list.pop(current_index)
+
+            close_list.append(current_node)
+
+            if current_node.position == self.end_node.position:
+                path  = []
+                while current_node is not None:
+                    path.append(current_node.position)
+                    current_node = current_node.parent
+
+
+                gui_reference.insert_path(self.start_node.position[1],self.start_node.position[0],"white")
+                gui_reference.insert_path(self.end_node.position[1],self.end_node.position[0],"red")
+
+
+                return path[::-1]  # Return reversed path
+
+            #Neightbours of current node
+            neighbours = []
+            for i in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
+
+                #each node position
+                node_neighbout_index = (current_node.position[0]+i[0], current_node.position[1] + i[1])
+
+                # see if its valid
+                if node_neighbout_index[0] > 15 or node_neighbout_index[0] < 0 or node_neighbout_index[1] > 15 or node_neighbout_index[1] < 0:
+                    continue
+                # if the value and the index = 3 it means that its not a valid move
+                if self.val_list[node_neighbout_index[0]][node_neighbout_index[1]] == 3:
+                    continue
+
+                # Create new node
+                new_node = Node(current_node, node_neighbout_index)
+
+                # Append
+                neighbours.append(new_node)
+
+            # see if neighbouts exist either in open list or close list
+            for neighbour in neighbours:
+                cancel_below_line = False
+
+
+                # colour in the selected neighbours it searching
+                gui_reference.insert_path(neighbour.position[1],neighbour.position[0],"green")
+
+                # gui_reference.start_node_bool = True
+                # gui_reference.end_node_bool = True
+                #
+                # gui_reference.draw_start_end_nodes(self.start_node.position[1],self.start_node.position[0])
+                # gui_reference.draw_start_end_nodes(self.end_node.position[1], self.end_node.position[0])
+                #
+
+
+                for close_neighbours in close_list:
+                    if neighbour.position == close_neighbours.position:
+                        cancel_below_line = True
+                        break
+                if not cancel_below_line:
+                    # create f,g,h
+                    neighbour.g = neighbour.parent.g + 1  # since we are next to it
+                    neighbour.h = ((neighbour.position[0] - self.end_node.position[0]) ** 2 + (
+                                neighbour.position[1] - self.end_node.position[1]) ** 2)
+                    neighbour.f = neighbour.h + neighbour.g
+                    # Child is already in the open list
+                    for open_node in open_list:
+                        if neighbour.position == open_node.position and neighbour.g > open_node.g:
+                            #continue
+                            break
+                    # Add the child to the open list
+                    open_list.append(neighbour)
+
+
+
+
+        pass
+
+
+if __name__ == "__main__":
+    test = Logic_AStar_Algo()
+    test.print_neat()
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from tkinter import *
+import a_star_logic as logicBoard
 
 root = Tk()
 screen = Canvas(root, width=800, height=700, background="#222",highlightthickness=0)
 screen.pack()
+
+
 
 class gui:
     def __init__(self):
@@ -10,52 +13,59 @@ class gui:
         Creates a new board
         """
         self.new_board()
-        self.start_node = True
-        self.end_node = True
+        self.start_node_bool = True
+        self.end_node_bool = True
+        self.index_node =[]
 
     def new_board(self):
         # Horizontal Line
+        for i in range(0,17):
+            screen.create_line(200, 150+i*25,600, 150+i*25,fill = "red")
 
-        screen.create_line(200, 150, 600, 150, fill="red")
-        screen.create_line(200, 200, 600, 200, fill="red")
-        screen.create_line(200, 250, 600, 250, fill="red")
-        screen.create_line(200, 300, 600, 300, fill="red")
-        screen.create_line(200, 350, 600, 350, fill="red")
-        screen.create_line(200, 400, 600, 400, fill="red")
-        screen.create_line(200, 450, 600, 450, fill="red")
-        screen.create_line(200, 500, 600, 500, fill="red")
-        screen.create_line(200, 550, 600, 550, fill="red")
+        for j in range(0,17):
+            screen.create_line(200+j*25,150,200+j*25,550,fill = "red")
+
+        # screen.create_line(200, 150, 600, 150, fill="red")
+        # screen.create_line(200, 200, 600, 200, fill="red")
+        # screen.create_line(200, 250, 600, 250, fill="red")
+        # screen.create_line(200, 300, 600, 300, fill="red")
+        # screen.create_line(200, 350, 600, 350, fill="red")
+        # screen.create_line(200, 400, 600, 400, fill="red")
+        # screen.create_line(200, 450, 600, 450, fill="red")
+        # screen.create_line(200, 500, 600, 500, fill="red")
+        # screen.create_line(200, 550, 600, 550, fill="red")
+        # screen.create_line(200, 175, 600, 175, fill="red")
+        # screen.create_line(200, 225, 600, 225, fill="red")
+        # screen.create_line(200, 275, 600, 275, fill="red")
+        # screen.create_line(200, 325, 600, 325, fill="red")
+        # screen.create_line(200, 375, 600, 375, fill="red")
+        # screen.create_line(200, 425, 600, 425, fill="red")
+        # screen.create_line(200, 475, 600, 475, fill="red")
+        # screen.create_line(200, 525, 600, 525, fill="red")
 
         # Vertical Line
 
-        screen.create_line(200, 150, 200, 550, fill="red")
-        screen.create_line(250, 150, 250, 550, fill="red")
-        screen.create_line(300, 150, 300, 550, fill="red")
-        screen.create_line(350, 150, 350, 550, fill="red")
-        screen.create_line(400, 150, 400, 550, fill="red")
-        screen.create_line(450, 150, 450, 550, fill="red")
-        screen.create_line(500, 150, 500, 550, fill="red")
-        screen.create_line(550, 150, 550, 550, fill="red")
-        screen.create_line(600, 150, 600, 550, fill="red")
+        # screen.create_line(200, 150, 200, 550, fill="red")
+        # screen.create_line(250, 150, 250, 550, fill="red")
+        # screen.create_line(300, 150, 300, 550, fill="red")
+        # screen.create_line(350, 150, 350, 550, fill="red")
+        # screen.create_line(400, 150, 400, 550, fill="red")
+        # screen.create_line(450, 150, 450, 550, fill="red")
+        # screen.create_line(500, 150, 500, 550, fill="red")
+        # screen.create_line(550, 150, 550, 550, fill="red")
+        # screen.create_line(600, 150, 600, 550, fill="red")
+        #
+        # # new Addition Vertival
+        # screen.create_line(225, 150, 225, 550, fill="red")
+        # screen.create_line(275, 150, 275, 550, fill="red")
+        # screen.create_line(325, 150, 325, 550, fill="red")
+        # screen.create_line(375, 150, 375, 550, fill="red")
+        # screen.create_line(425, 150, 425, 550, fill="red")
+        # screen.create_line(475, 150, 475, 550, fill="red")
+        # screen.create_line(525, 150, 525, 550, fill="red")
+        # screen.create_line(575, 150, 575, 550, fill="red")
 
-        # new Addition Vertival
-        screen.create_line(225, 150, 225, 550, fill="red")
-        screen.create_line(275, 150, 275, 550, fill="red")
-        screen.create_line(325, 150, 325, 550, fill="red")
-        screen.create_line(375, 150, 375, 550, fill="red")
-        screen.create_line(425, 150, 425, 550, fill="red")
-        screen.create_line(475, 150, 475, 550, fill="red")
-        screen.create_line(525, 150, 525, 550, fill="red")
-        screen.create_line(575, 150, 575, 550, fill="red")
 
-        screen.create_line(200, 175, 600, 175, fill="red")
-        screen.create_line(200, 225, 600, 225, fill="red")
-        screen.create_line(200, 275, 600, 275, fill="red")
-        screen.create_line(200, 325, 600, 325, fill="red")
-        screen.create_line(200, 375, 600, 375, fill="red")
-        screen.create_line(200, 425, 600, 425, fill="red")
-        screen.create_line(200, 475, 600, 475, fill="red")
-        screen.create_line(200, 525, 600, 525, fill="red")
 
         self.initial_text =screen.create_text(400, 620, fill ="white", text= " Click on two places, start and end respectively.")
         screen.update()
@@ -69,36 +79,63 @@ class gui:
         if x_val>15 or x_val<0 or y_val>15 or y_val <0:
             return
 
-        if self.start_node:
+        if self.start_node_bool:
             print("test2")
             #screen.create_rectangle(200, 150, 225, 200,                                    fill="White")
             screen.create_rectangle(200+(x_val*25),150+(y_val*25),225+(x_val*25),175+(y_val*25), fill ="White")
-            self.start_node = False
+            self.start_node_bool = False
+            self.index_node += [(x_val,y_val)]
 
-        elif self.end_node:
-            screen.create_rectangle(200 + (x_val * 25), 150 + (y_val * 25), 225 + (x_val * 25), 175 + (y_val * 25),
-                                    fill="red")
-            self.end_node = False
+            board.insert_Nodes(x_val, y_val, 1)
+
+
+
+
+        elif self.end_node_bool:
+            ## Cannot override existing start Node
+            if (x_val, y_val) not in self.index_node:
+                screen.create_rectangle(200 + (x_val * 25), 150 + (y_val * 25), 225 + (x_val * 25), 175 + (y_val * 25),
+                                        fill="red")
+                self.end_node_bool = False
+                self.index_node += [(x_val, y_val)]
+                board.insert_Nodes(x_val, y_val, 2)
+
+
+
+
+
+
         else:
-            screen.create_rectangle(200 + (x_val * 25), 150 + (y_val * 25), 225 + (x_val * 25), 175 + (y_val * 25),
-                                    fill="blue")
+            if (x_val,y_val) not in self.index_node:
+                screen.create_rectangle(200 + (x_val * 25), 150 + (y_val * 25), 225 + (x_val * 25), 175 + (y_val * 25),
+                                        fill="blue")
+
+
+            board.insert_Nodes(x_val, y_val, 0)
             return
 
-        if not interface.end_node and not interface.start_node:
+        if not interface.end_node_bool and not interface.start_node_bool:
             interface.remove_initial_text()
     def clear_board(self,):
         screen.delete("all")
         self.new_board()
-        self.start_node = True
-        self.end_node = True
+        self.start_node_bool = True
+        self.end_node_bool = True
+        self.index_node = []
 
-    # def test(self,x,y):
-    #     print(x>15 or x<15 or y>15 or y <15)
-    #     if x>15 or x<0 or y>15 or y <0:
-    #         return
-    #
-    #     screen.create_rectangle(200 + (x* 25), 150 + (y * 25), 225 + (x * 25), 175 + (y * 25),
-    #                             fill="blue")
+
+
+
+    def insert_path(self,x_val,y_val,colour):
+        screen.create_rectangle(200 + (x_val * 25), 150 + (y_val * 25), 225 + (x_val * 25), 175 + (y_val * 25),
+                                fill=colour)
+
+
+
+
+
+
+
 
 
 
@@ -128,11 +165,36 @@ def hold_left_click(event):
     interface.test(rounded_x,rounded_y)
     print(rounded_x, rounded_y)
 def clear1(event):
+    global board
     print("test")
     interface.clear_board()
+    board = logicBoard.Logic_AStar_Algo()
+    print(board.print_neat())
+
+# Will remove later
+def print_clear(event):
+    board.print_neat()
+    print("-----------------------------------------------------------")
+
+
+def start_Astar_Algo(event):
+    x=board.pathFinding(interface)
+    print(x)
+    # pop first element and second element
+    try:
+        x.pop(0)
+        x.pop(-1)
+    except:
+        pass
+    for valid_pos in x:
+        interface.insert_path(valid_pos[1],valid_pos[0],"orange")
+
 
 if __name__ == "__main__":
+    global board
+    board = logicBoard.Logic_AStar_Algo()
 
+    print(board.print_neat())
     interface = gui()
     # Bind the left click
     screen.bind("<Button-1>", left_click)
@@ -140,6 +202,8 @@ if __name__ == "__main__":
     #Make a focus set so users have acces to key with canvas
     screen.focus_set()
     screen.bind("c", clear1)
+    screen.bind("p", print_clear)
+    screen.bind("s",start_Astar_Algo)
 
 
     root.mainloop()
